95da5e907b93a913d414bcf670bbfa9eafcb8ba7
spguy <spkinguy1@gmial.com>
2022-03-04 10:20:45 +0330

    seperate css , fix toggle like dislike

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 //pick up any div within footers
 const divs = document.querySelectorAll(".footer div");
-const like = document.querySelector(".footer div:nth-child(2)");
-const dislike = document.querySelector(".footer div:nth-child(4)");
+const table = document.querySelector(".collabse");
+const like = document.querySelectorAll(".footer div:nth-child(2)");
+const dislike = document.querySelectorAll(".footer div:nth-child(4)");
+const button = document.querySelector("section");
+const textarea = document.querySelector(".creator-wrapper");
+const insert = document.querySelector("button");
 
-like.addEventListener("click", likeHandller);
+const textBox = document.querySelector("textarea");
 
-dislike.addEventListener('click',dislikeHandler)
+insert.addEventListener("click", () => {
+    let post = document.createElement("article");
+    post.innerHTML = `
+        <a href="#"
+        ><img
+            src="images/download2.jpeg"
+            alt="username"
+        /></a>
+        <p class="content">
+        ${textBox.value}
+        </p>
+        <footer class="footer">
+            <div title="comments">A</div>
+            <div title="like">B</div>
+            <div title="republish">E</div>
+            <div title="dislike">I</div>
+            <div title="information">J</div>
+        </footer>
+`;
 
+    table.children[0].children[0].appendChild(post);
+});
 
+button.addEventListener("click", () => {
+    if (!textarea.classList.contains("none")) {
+        textarea.classList.add("none");
+    } else {
+        textarea.classList.remove("none");
+    }
+});
 
-
-
-function dislikeHandler() {
-    if (dislike.textContent == "I")
-    dislike.textContent = "D";
+for (let li of like) {
+    li.addEventListener("click", () => {
+        if (li.textContent == "B") {
+            li.textContent = "C";
+        } else if (li.textContent == "C") {
+            li.textContent = "B";
+        }
+    });
 }
 
-
-function likeHandller() {
-        if (like.textContent == "B")
-        like.textContent = "C";
+for (let dis of dislike) {
+    dis.addEventListener("click", () => {
+        if (dis.textContent == "I") {
+            dis.textContent = "D";
+        } else if (dis.textContent == "D") {
+            dis.textContent = "I";
+        }
+    });
 }
 
-
+function dislikeHandler() {}
 
 function createArticle({ post }) {}
